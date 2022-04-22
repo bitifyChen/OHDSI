af9177b70bd3318b1bbe67411ac28751b6e516be
bitifyChen <43808882+bitifyChen@users.noreply.github.com>
2022-04-22 17:47:01 +0800

    220422-menu V1

--- main.js
+++ main.js
@@ -1,74 +1,26 @@
 /*Nav-mobile*/
-const btnMenu = document.querySelector("header.main button.menu");
-const menu = document.querySelector("header.main");
+const btnMenu = document.querySelector(".main-header .btn-menu");
+const menu = document.querySelector(".main-header");
 const navLinks = document.querySelectorAll(".main-nav .link");
+const navDropLinks = document.querySelectorAll(".main-nav .link.dropdown");
+
 btnMenu.addEventListener("click", menuOpen);
 function menuOpen() {
   menu.classList.toggle("active");
-  document.querySelector("body").classList.toggle("lock-scroll");
 }
-/*--點擊link後，清除active效果，防止返回時看到*/
-navLinks.forEach((link) => {
+//--下拉式選單
+navDropLinks.forEach((link) => {
   link.addEventListener("click", () => {
-    menu.classList.remove("active");
+    if (link.classList.contains("open")) {
+      //關閉本身下拉選單
+      link.classList.remove("open");
+    } else {
+      //關閉已開啟下拉選單
+      navDropLinks.forEach((link) => {
+        link.classList.remove("open");
+      });
+      //開啟本身下拉選單
+      link.classList.add("open");
+    }
   });
 });
-
-/*Nav-desktop-紅球跟隨效果*/
-const navBar = document.querySelector(".main-nav .links");
-const dot = document.querySelector(".dot");
-let dotReload = true;
-if (dot) {
-  navBar.addEventListener("mouseover", () => {
-    dot.classList.add("moving");
-  });
-  navBar.addEventListener("mouseout", () => {
-    dot.classList.remove("moving");
-  });
-  navLinks.forEach((link) => {
-    link.addEventListener("mouseover", () => {
-      dot.classList.remove("with-position");
-      dot.classList.add("moving");
-      dot.style.setProperty("--move", link.offsetLeft - dot.offsetLeft + "px");
-    });
-  });
-}
-/*--進頁面後判斷*/
-/*---跑一次---*/
-if (dotReload) {
-  navLinks.forEach((link) => {
-    /*---確認有已啟動*/
-    if (link.classList.contains("active")) {
-      dot.style.setProperty("--move", link.offsetLeft - dot.offsetLeft + "px");
-      dot.classList.add("with-position"); //無轉場時間，讓紅點不飄移
-      dot.classList.add("moving");
-    }
-    /*---判斷換頁後滑鼠在不在navbar */
-    document.querySelector("body").addEventListener("mousemove", () => {
-      if (!menu.matches(":hover")) {
-        dot.classList.remove("moving");
-        dot.classList.remove("with-position");
-      }
-    });
-  });
-  dotReload = false;
-}
-
-/*Back to Previous Page 返回上一頁*/
-const btnBack = document.querySelectorAll(".leave button.back");
-if (btnBack) {
-  btnBack.forEach((btn) => {
-    btn.addEventListener("click", backToPrevious);
-  });
-}
-function backToPrevious() {
-  window.history.back();
-}
-
-/*Safari-100VH問題*/
-function safariHacks() {
-  let windowsVH = window.innerHeight / 100;
-  document.querySelector("main").style.setProperty("--vh", windowsVH + "px");
-}
-window.addEventListener("resize", safariHacks);
-safariHacks();
